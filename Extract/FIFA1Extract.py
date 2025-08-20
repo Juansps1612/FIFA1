20b5e1cfc2f96d31097cf98c12a1699343030706
Juansps1612 <148258868+Juansps1612@users.noreply.github.com>
2025-08-20 15:48:12 +0000

    se creo estructura

---
+++ Extract/FIFA1Extract.py
@@ -0,0 +1,10 @@
+import requests
+
+URL = "https://ipinfo.io/190.60.194.114/json"
+
+try: 
+    response = requests.get(URL)
+    data = response.json
+    print(data)
+except:
+    print("hubo un error")
